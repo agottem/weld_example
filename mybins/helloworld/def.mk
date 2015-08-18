86db2469bfdc63f0ace808e36ba1209b8043ea38
Andrew Gottemoller <http://www.agottem.com>
2015-08-18 12:04:55 -0500

    Add helloworld binary example which customizes include and source directories

---
+++ mybins/helloworld/def.mk
@@ -0,0 +1,12 @@
+name := helloworld
+lang := c
+type := bin
+
+def_source_subdir   := src
+def_include_subdir  := inc
+def_resource_subdir := res
+
+
+source_list   := main.c
+header_list   := main.h
+resource_list := test.dir.rename.res
